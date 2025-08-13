7f504d87de1e945427b703a60febcad73c6e315d
c00kie86 <86kowalczyk@gmail.com>
2025-08-13 05:34:59 +0200

    chore(src): Update and added controllers

--- src/App/Controller/DefaultController.php
+++ src/App/Controller/DefaultController.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 
@@ -18,20 +17,5 @@ class DefaultController extends AbstractController
             'app-root.html.twig'
         );
     }
-
-    public function setupCheck(Request $request): Response
-    {
-        $responseContent = json_encode([
-            'testParam' => $request->get('testParam')
-                ? (int) $request->get('testParam')
-                : null
-        ]);
-        return new Response(
-            $responseContent,
-            Response::HTTP_OK,
-            ['Content-type' => 'application/json']
-        );
-    }
-
 
 }
