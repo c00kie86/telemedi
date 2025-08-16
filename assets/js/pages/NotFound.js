9b66f100c30a8b44a93066f1bb2f32091b608ff0
c00kie86 <86kowalczyk@gmail.com>
2025-08-17 01:53:07 +0200

    feat(assets/js/pages): Update NotFound.js

---
+++ assets/js/pages/NotFound.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <>
+      <main>
+        <section className="container-md">
+          <div className="row mt-4">
+            <div className="col col-md-8 mx-auto">
+              <h2 className="text-center"><span>404</span> - Nie znaleziono strony</h2>
+            </div>
+          </div>
+        </section>
+      </main>
+    </>
+  );
+};
+
+export default NotFound;
