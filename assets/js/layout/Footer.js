0145543be199bc1011bbf3a0f5e048858b5c3859
c00kie86 <86kowalczyk@gmail.com>
2025-08-17 01:57:09 +0200

    feat(assets/js/layout): Added Footer.js

---
+++ assets/js/layout/Footer.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <>
+      <footer>
+        <section className="container-md">
+          <div className="row mt-2">
+            <div className="col-md-8 d-flex justify-content-center align-items-center mx-auto border-top">
+              <span className="my-4">© 2025 c00kie86, Inc</span>
+            </div>
+          </div>
+        </section>
+      </footer>
+    </>
+  );
+}
+
+export default Footer;
