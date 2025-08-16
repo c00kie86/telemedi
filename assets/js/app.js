315c45145f10cf599781e101ef1403d9ab69a6a0
c00kie86 <86kowalczyk@gmail.com>
2025-08-17 01:25:21 +0200

    chore(assets/js): Update app.js import ./views/App

--- assets/js/app.js
+++ assets/js/app.js
@@ -15,7 +15,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import '../css/app.css';
-import App from './components/App';
+import App from './views/App';
 
-ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
-
+const rootElement = document.getElementById('root');
+if (rootElement) {
+    ReactDOM.render(
+        <Router>
+            <App />
+        </Router>,
+        rootElement
+    );
+}
