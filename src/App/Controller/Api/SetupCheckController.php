fd48a89739c1c982435daa7a61e677bc0c838dde
c00kie86 <86kowalczyk@gmail.com>
2025-08-17 02:37:35 +0200

    refactor(src/App/Controller/Api): Update SetupCheckController.php

--- src/App/Controller/Api/SetupCheckController.php
+++ src/App/Controller/Api/SetupCheckController.php
@@ -1,27 +1,22 @@
 <?php
-// src/Controller/Api/SetupCheckController.php
-declare(strict_types=1);
 
 namespace App\Controller\Api;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
-
 
 class SetupCheckController extends AbstractController
 {
-    public function setupCheck(Request $request): Response
+    public function setupCheck(Request $request): JsonResponse
     {
-        $responseContent = json_encode([
-            'testParam' => $request->get('testParam')
-                ? (int) $request->get('testParam')
-                : null
-        ]);
-        return new Response(
-            $responseContent,
-            Response::HTTP_OK,
-            ['Content-type' => 'application/json']
-        );
+        $testParam = $request->get('testParam');
+        
+        $responseContent = [
+            'testParam' => $testParam !== null ? (int) $testParam : null
+        ];
+
+        return $this->json($responseContent, Response::HTTP_OK);
     }
 }
